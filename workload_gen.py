2049d4cddd69b932c1c381cb30b1d3e8f9204056
msrba <maik.srba@gwdg.de>
2017-06-23 10:59:47 +0200

    Update workload_gen.py

--- workload_gen.py
+++ workload_gen.py
@@ -5,9 +5,9 @@ logger = logging.getLogger(__name__)
 
 
 def run(context):
-    workload_config = context.v1.self.config
+    workload_config = context.v1.workload.config
     workload_params = workload_config['params']
-    print '{greeting}'.format(greeting=workload_params['greeting'])
-    print 'I\'m a workload generator, my name is {name} '.format(name=workload_config['name']),
+    print '{}'.format(workload_params['greeting'])
+    print 'I\'m a workload generator, my name is {} '.format(context.workload.name),
     print 'and I\'m running a demo workload with my config:'
-    print '{workload_config}'.format(workload_config=workload_config)
+    print '{}'.format(workload_config)
