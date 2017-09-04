0a499a41294eb5b7e1df6922daa3c44ccec6ae70
Maik Srba <msrba@gwdg.de>
2017-09-04 10:25:03 +0200

    update demo

--- workload_gen.py
+++ workload_gen.py
@@ -4,16 +4,16 @@ from scotty import utils
 
 logger = logging.getLogger(__name__)
 
-
 def result(context):
-    pass
+    return true
 
 def run(context):
     workload = context.v1.workload
-    experiment_helper = utils.ExperimentHelper(context)
-    demo_resource = experiment_helper.get_resource(workload.resources['demo_res'])
-    print '{}'.format(workload.params['greeting'])
-    print 'I\'m a workload generator, my name is {} '.format(workload.name),
-    print 'and I\'m running a demo workload with my config:'
-    print '{}'.format(workload.config)
-    print 'The demo resource endpoint is: {}'.format(demo_resource.endpoint)
+    exp_helper = utils.ExperimentHelper(context)
+#    my_resource = exp_helper.get_resource(workload.resources['my_resource']
+    logger.info('{}'.format(workload.params['greeting']))
+    logger.info('I\'m workload generator {}'.format(workload.name))
+#    logger.info('The resource endpoint is {}'.format(my_resource))
+
+def clean(context):
+    pass
