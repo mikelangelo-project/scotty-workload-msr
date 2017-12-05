e57dca58ade1ce99474b0812e1f5490d08fd5f57
Peter Chronz <peter.chronz@gmail.com>
2017-12-05 17:03:51 +0100

    Add pip installation for pymongo

--- workload_gen.py
+++ workload_gen.py
@@ -1,17 +1,23 @@
 import logging
+import pip
 
 from scotty import utils
 
+pip.main(['install', 'pymongo'])
+import pymongo
+
 logger = logging.getLogger(__name__)
+
 
 def run(context):
     workload = context.v1.workload
     exp_helper = utils.ExperimentHelper(context)
-#    my_resource = exp_helper.get_resource(workload.resources['my_resource']
+    #    my_resource = exp_helper.get_resource(workload.resources['my_resource']
     logger.info('{}'.format(workload.params['greeting']))
     logger.info('I\'m workload generator {}'.format(workload.name))
-#    logger.info('The resource endpoint is {}'.format(my_resource))
+    #    logger.info('The resource endpoint is {}'.format(my_resource))
     return result_description
+
 
 def clean(context):
     pass
