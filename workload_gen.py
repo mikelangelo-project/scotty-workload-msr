748148feac215f63d0b9f0bcd803d843f4671c9c
Peter Chronz <peter.chronz@gmail.com>
2017-12-06 16:39:33 +0100

    Fix output for mongo password

--- workload_gen.py
+++ workload_gen.py
@@ -13,7 +13,8 @@ def run(context):
     logger.info('{}'.format(workload.params['greeting']))
     mongo_user = workload.params['mongo_user']
     mongo_password = workload.params['mongo_password']
-    logger.info(mongo_user, mongo_password)
+    logger.info(mongo_user)
+    logger.info(mongo_password)
     pymongo.MongoClient()
     logger.info('I\'m workload generator {}'.format(workload.name))
     return None
